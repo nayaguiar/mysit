5f4fc7a5d51236dcc29afdfbdf38b82a2e7cdd89
nayara lopes <nayaralp1962@gmail.com>
2019-02-01 21:25:47 -0200

    first commit

---
+++ core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome','email','telefone')
+
+admin.site.register(Contato, ContatoAdmin)
+
+# Register your models here.
---
+++ core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    data_nascimento = models.DateField()
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+
+class ItemAgenda(models.Model):
+    data = models.DateField()
+    hora = models.TimeField()
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+# Create your models here.
